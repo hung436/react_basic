0647c8186eeea0502bcd4f0f9d4f915a4c032b5d
hung <ttldhung@gmail.com>
2022-04-04 15:38:05 +0700

    sửa nav.link header Admin

--- src/component/AdminPage/component/HeaderAD.jsx
+++ src/component/AdminPage/component/HeaderAD.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Navbar,Nav,Container} from 'react-bootstrap'
+import {Navbar,Nav,Container,NavDropdown} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 import {adminLogout} from './adminSlice'
 import {useDispatch} from 'react-redux'
-import {NavLink} from 'react-router-dom'
+import {NavLink,Link} from 'react-router-dom'
 export default function HeaderAD(props) {
   const dispatch = useDispatch()
   const handleLogout = () => {
@@ -12,36 +12,36 @@ export default function HeaderAD(props) {
   return (
     <div>
  
- <Navbar bg="light" expand="lg">
+ <Navbar bg="primary" variant='dark' expand="lg" sticky="top">
   <Container>
-    <Navbar.Brand><NavLink to="/">Trang chủ</NavLink></Navbar.Brand>
+    <Navbar.Brand as={Link} to="/">Trang chủ</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar.Collapse id="basic-navbar-nav ">
       <Nav className="me-auto">
        
-            <NavLink to="/admin"  onClick={() => {
+            <Nav.Link as={Link} to="/admin"  onClick={() => {
             props.reload();
-          }}> Dashboard</NavLink>
+          }}> Dashboard</Nav.Link>
 
-         <NavLink to="/admin/category"  onClick={() => {
+         <Nav.Link as={Link} to="/admin/category"  onClick={() => {
             props.reload();
-          }}> Danh mục</NavLink>
-         <NavLink to="/admin/product"  onClick={() => {
+          }}> Danh mục</Nav.Link>
+         <Nav.Link as={Link} to="/admin/product"  onClick={() => {
             props.reload();
-          }}>Sản phẩm</NavLink>
-        <NavLink to="/admin/user"  onClick={() => {
+          }}>Sản phẩm</Nav.Link>
+        <Nav.Link as={Link} to="/admin/user"  onClick={() => {
             props.reload();
-          }}> Người dùng</NavLink>
-<button className="btn btn-primary" onClick={handleLogout}>Dang xuat</button>
-        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown> */}
+          }}> Người dùng</Nav.Link>
+     
       </Nav>
-
+     
+      <NavDropdown title={ <i className="fa-solid fa-circle-user fs-3 text-light"></i>} id="basic-nav-dropdown" >
+          <NavDropdown.Item href="#action/3.1">Thông tin</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Cài đặt</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Language</NavDropdown.Item>
+          {/* <NavDropdown.Divider /> */}
+          <NavDropdown.Item href="#action/3.4" onClick={handleLogout}>Đăng xuất</NavDropdown.Item>
+        </NavDropdown>
     </Navbar.Collapse>
   </Container>
 </Navbar>
